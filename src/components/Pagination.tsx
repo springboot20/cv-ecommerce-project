77a7cdaa16cc7e781931d9a0fbc2a850ad084336
Yunus Abbas Opeyemi <opeyemiakanbi328@gmail.com>
2024-06-21 18:28:53 +0100

    pagination component fixed

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -1,3 +1,5 @@
+import React from 'react'
+import { Button, IconButton, IconButtonProps } from '@material-tailwind/react'
 import { ArrowLeftIcon, ArrowRightIcon } from '@heroicons/react/24/outline'
 
 export const Pagination: React.FC<{
@@ -6,59 +8,60 @@ export const Pagination: React.FC<{
   currentPage: number
   onPageChange: React.Dispatch<React.SetStateAction<number>>
 }> = ({ itemsPerPage, totalItems, currentPage, onPageChange }) => {
-  const totalPages = Math.ceil(totalItems! / itemsPerPage)
+  const totalPages = Math.ceil((totalItems || 0) / itemsPerPage)
 
   const handlePageChange = (page: number) => {
     if (page >= 1 && page <= totalPages) {
       onPageChange(page)
     }
   }
+
+  const getItemProps = (index: number) => ({
+    variant:
+      currentPage === index ? 'filled' : ('text' as IconButtonProps['variant']),
+    color: 'gray' as IconButtonProps['color'],
+    onClick: () => handlePageChange(index),
+    children: index + 1,
+  })
+
+  const next = () => {
+    if (currentPage < totalPages) {
+      handlePageChange(currentPage + 1)
+    }
+  }
+
+  const prev = () => {
+    if (currentPage > 1) {
+      handlePageChange(currentPage - 1)
+    }
+  }
+
   return (
-    <div className=" absolute bottom-7 mt-32 left-2/4 rounded-lg bg-white dark:bg-dark-blue-dark-DME mx-auto -translate-x-2/4">
-      <div className="flex items-center lg:px-4 py-2 rounded-md gap-[0.15rem] lg:gap-9">
-        <button
-          onClick={() => handlePageChange(currentPage - 1)}
-          className={
-            'flex items-center gap-5 p-2 cursor-pointer mr-4'
-          }
+    <div className="absolute bottom-7 mt-32 left-2/4 rounded-lg bg-white dark:bg-dark-blue-dark-DME mx-auto -translate-x-2/4">
+      <div className="flex items-center lg:px-4 py-2 rounded-md gap-4">
+        <Button
+          variant="text"
+          className="flex items-center gap-2"
+          onClick={prev}
+          disabled={currentPage === 1}
         >
-          <ArrowLeftIcon
-            className={
-              'h-6 text-dark-blue-dark-LMT hidden md:block dark:text-white'
-            }
-          />
-          <span className="text-md md:text-xl font-semibold block dark:text-white">
-            Previous
-          </span>
-        </button>
-        {Array.from({ length: totalPages }, (_, index) => (
-          <button
-            key={index}
-            onClick={() => handlePageChange(index + 1)}
-            className={`p-[0.30rem] sm:p-[0.55rem] lg:p-3 h-full block font-semibold text-lg cursor-pointer rounded-md transition-all ${
-              currentPage === index + 1
-                ? 'text-blue-700 bg-gray-300'
-                : 'text-gray-500 dark:text-white'
-            }`}
-          >
-            {index + 1}
-          </button>
-        ))}
-        <button
-          onClick={() => handlePageChange(currentPage + 1)}
-          className={
-            'flex items-center gap-3 p-2 cursor-pointer'
-          }
+          <ArrowLeftIcon strokeWidth={2} className="h-4 w-4" /> Previous
+        </Button>
+        <div className="flex items-center gap-2">
+          {Array.from({ length: totalPages }, (_, index) => (
+            <IconButton key={index} {...getItemProps(index + 1)}>
+              {index + 1}
+            </IconButton>
+          ))}
+        </div>
+        <Button
+          variant="text"
+          className="flex items-center gap-2"
+          onClick={next}
+          disabled={currentPage === totalPages}
         >
-          <span className="text-md md:text-xl font-semibold block dark:text-white">
-            Next
-          </span>
-          <ArrowRightIcon
-            className={
-              'h-6 text-dark-blue-dark-LMT dark:text-white hidden md:block'
-            }
-          />
-        </button>
+          Next <ArrowRightIcon strokeWidth={2} className="h-4 w-4" />
+        </Button>
       </div>
     </div>
   )
