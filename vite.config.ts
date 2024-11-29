80e5a3a10db64dadbafa9b70376310da2b76bd90
springboot20 <abbasopeyemi148@gmail.com>
2024-11-29 23:35:19 +0100

    modified

--- vite.config.ts
+++ vite.config.ts
@@ -4,14 +4,14 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      "/api": {
-        target: "https://e-commerce-api-6wjr.onrender.com",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""),
-        secure: false,
-      },
-    },
-  },
+  // server: {
+  //   proxy: {
+  //     "/api": {
+  //       target: "https://e-commerce-api-6wjr.onrender.com",
+  //       changeOrigin: true,
+  //       rewrite: (path) => path.replace(/^\/api/, ""),
+  //       secure: false,
+  //     },
+  //   },
+  // },
 });
