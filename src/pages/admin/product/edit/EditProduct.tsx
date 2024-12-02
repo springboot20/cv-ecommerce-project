18382ae03ca431029693460306bec1567fdd56fa
springboot20 <abbasopeyemi148@gmail.com>
2024-12-03 00:53:37 +0100

    modified admin edit product page component

--- src/pages/admin/product/edit/EditProduct.tsx
+++ src/pages/admin/product/edit/EditProduct.tsx
@@ -68,7 +68,7 @@ export default function EditProduct() {
     price: product?.price ?? 10,
     description: product?.description ?? "",
     imageSrc: product?.imageSrc ?? selectedFile,
-    category: product?.category ?? ({_id:"", name:""}),
+    category: product?.category ?? { _id: "", name: "" },
     stock: product?.stock ?? 1,
     featured: product?.featured ?? false,
     name: product?.name ?? "",
@@ -180,7 +180,7 @@ export default function EditProduct() {
                         "block w-full  rounded border-0 p-3 text-gray-700 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset text-sm outline-none",
                         errors.name && touched.name ? "ring-red-500" : "focus:ring-indigo-500",
                       )}
-                      onChange={(event) => {
+                      onChange={(event: any) => {
                         const selectedCategory = categories?.find(
                           (category) => category.name === event.target.value,
                         );
