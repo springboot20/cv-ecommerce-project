b360dbfe2b9adc8025974b75b5aa20dae327712c
springboot20 <abbasopeyemi148@gmail.com>
2024-12-04 13:11:46 +0100

    packages updated

--- src/pages/check-out/verify/VerifyPayment.tsx
+++ src/pages/check-out/verify/VerifyPayment.tsx
@@ -1,9 +1,9 @@
 import { useEffect } from "react";
+import axios from "axios";
 
 export default function VerifyPaystackPayment() {
   useEffect(() => {
-
-
+    const req = axios.get(``)
   }, []);
 
   return <div className=""></div>;
