33bfb60c45423de0ff4d9104b6ecf8922a2c92ae
springboot20 <abbasopeyemi148@gmail.com>
2024-12-04 13:30:09 +0100

    added paystack verification

--- src/pages/check-out/verify/VerifyPayment.tsx
+++ src/pages/check-out/verify/VerifyPayment.tsx
@@ -1,9 +1,32 @@
 import { useEffect } from "react";
 import axios from "axios";
 
+let env = import.meta.env;
+
+let base_url = env.MODE === "development" ? env.VITE_API_BASE_URL_DEV : env.VITE_API_BASE_URL_PROD;
+
+const verifyPaystackPayment = async (trxref: string, reference: string) => {
+  try {
+    const response = await axios.get(`${base_url}/orders/provider/paystack/verify-callback`, {
+      params: {
+        trxref,
+        reference,
+      },
+    });
+
+    console.log(response);
+  } catch (error: any) {}
+};
+
 export default function VerifyPaystackPayment() {
   useEffect(() => {
-    const req = axios.get(``)
+    const urlParams = new URLSearchParams(window.location.search);
+    const trxref = urlParams.get("trxref");
+    const reference = urlParams.get("reference");
+
+    if (trxref && reference) {
+      verifyPaystackPayment(trxref, reference);
+    }
   }, []);
 
   return <div className=""></div>;
