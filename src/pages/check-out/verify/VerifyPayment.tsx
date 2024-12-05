153ac6cd09326b15aacbd77169c45474183e7a84
springboot20 <abbasopeyemi148@gmail.com>
2024-12-05 14:35:48 +0100

    added authorization

--- src/pages/check-out/verify/VerifyPayment.tsx
+++ src/pages/check-out/verify/VerifyPayment.tsx
@@ -1,13 +1,18 @@
 import { useEffect } from "react";
 import axios from "axios";
+import { useAppSelector } from "../../../hooks/redux/redux.hooks";
+import { RootState } from "../../../app/store";
 
 let env = import.meta.env;
 
 let base_url = env.MODE === "development" ? env.VITE_API_BASE_URL_DEV : env.VITE_API_BASE_URL_PROD;
 
-const verifyPaystackPayment = async (trxref: string, reference: string) => {
+const verifyPaystackPayment = async (trxref: string, reference: string, access_token: string) => {
   try {
     const response = await axios.get(`${base_url}/orders/provider/paystack/verify-callback`, {
+      headers: {
+        Authorization: `Bearer ${access_token}`,
+      },
       params: {
         trxref,
         reference,
@@ -19,13 +24,15 @@ const verifyPaystackPayment = async (trxref: string, reference: string) => {
 };
 
 export default function VerifyPaystackPayment() {
+  const { tokens } = useAppSelector((state: RootState) => state.auth);
+
   useEffect(() => {
     const urlParams = new URLSearchParams(window.location.search);
     const trxref = urlParams.get("trxref");
     const reference = urlParams.get("reference");
 
     if (trxref && reference) {
-      verifyPaystackPayment(trxref, reference);
+      verifyPaystackPayment(trxref, reference, tokens?.access_token!);
     }
   }, []);
 
