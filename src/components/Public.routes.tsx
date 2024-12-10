4525b99076bbfd63c54a978316e01095d17745a9
springboot20 <abbasopeyemi148@gmail.com>
2024-12-11 00:39:21 +0100

    optimized public route authentication component

--- src/components/Public.routes.tsx
+++ src/components/Public.routes.tsx
@@ -2,30 +2,25 @@ import { Navigate } from "react-router-dom";
 import { RootState } from "../app/store";
 import { useAppSelector } from "../hooks/redux/redux.hooks";
 
+const redirectIfAuthenticated = (isAuthenticated: boolean, redirectTo: string): React.ReactNode => {
+  if (isAuthenticated) {
+    return <Navigate to={redirectTo} state={{ path: window.location.pathname }} replace />;
+  }
+  return null;
+};
+
 const PublicRoute: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   const auth = useAppSelector((state: RootState) => state.auth);
 
-  if (auth.tokens) {
-    return <Navigate to="/" state={{ path: window.location.pathname }} replace={true} />;
-  }
-
-  return children;
+  return redirectIfAuthenticated(auth.tokens! && auth.isAuthenticated, "/") || children;
 };
 
 export const AdminPublicRoute: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   const auth = useAppSelector((state: RootState) => state.auth);
 
-  if (auth.admin) {
-    return (
-      <Navigate
-        to="/admin/overview"
-        state={{ path: window.location.pathname }}
-        replace={true}
-      />
-    );
-  }
-
-  return children;
+  return (
+    redirectIfAuthenticated(auth.admin! && auth.isAuthenticated, "/admin/overview") || children
+  );
 };
 
 export default PublicRoute;
