e886674285bce765566404006c79e642c62f79dd
Abbas opeyemi <100578205+springboot20@users.noreply.github.com>
2024-12-16 18:19:24 +0100

    Update FormContext.tsx
    updated context to keep track of the current step even when the page is reload

--- src/context/FormContext.tsx
+++ src/context/FormContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
 
 interface FormContextType {
   handleNextStep: () => void;
@@ -12,9 +12,16 @@ interface FormContextType {
 export const FormContext = createContext({} as FormContextType);
 
 export const FormProvider = ({ children }: { children: React.ReactNode }) => {
-  const [currentStep, setCurrentStep] = useState<number>(0);
+  // Retrieve the current step from localStorage, defaulting to 0 if not found
+  const initialStep = Number(localStorage.getItem("currentStep") || 0);
 
+  const [currentStep, setCurrentStep] = useState<number>(initialStep);
   const [steps, setSteps] = useState<JSX.Element[]>([]);
+
+  // Update localStorage whenever `currentStep` changes
+  useEffect(() => {
+    localStorage.setItem("currentStep", currentStep.toString());
+  }, [currentStep]);
 
   const handleNextStep = () => setCurrentStep((prev) => Math.min(prev + 1, steps.length - 1));
   const handlePrevStep = () => setCurrentStep((prev) => Math.max(prev - 1, 0));
